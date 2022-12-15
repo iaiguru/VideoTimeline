bf4f3740274ade53142e9cac5b34cd063f3cbbc0
irongru <j.doit926@gmail.com>
2022-12-15 11:58:13 -0600

    Initial Commit

---
+++ VideoTimeline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoTimeline
+//
+//  Created by hope on 12/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
